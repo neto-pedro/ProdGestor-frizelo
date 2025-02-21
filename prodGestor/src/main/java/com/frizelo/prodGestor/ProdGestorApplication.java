71b731161896f9492447b6163a038f7e834aa82d
Pedro Pires <pirespedro020@gmail.com>
2025-02-21 08:37:56 -0400

    feat: initial config

---
+++ prodGestor/src/main/java/com/frizelo/prodGestor/ProdGestorApplication.java
@@ -0,0 +1,13 @@
+package com.frizelo.prodGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdGestorApplication.class, args);
+	}
+
+}

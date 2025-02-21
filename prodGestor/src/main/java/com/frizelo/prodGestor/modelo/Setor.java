1daa0543a2c65a8554111a99cedd769eeebb5eb4
neto-pedro <pirepedro020@gmail.com>
2025-02-21 15:20:51 -0400

    feat: criando relacionamento entre Setor e Usuario

--- prodGestor/src/main/java/com/frizelo/prodGestor/modelo/Setor.java
+++ prodGestor/src/main/java/com/frizelo/prodGestor/modelo/Setor.java
@@ -3,11 +3,10 @@ package com.frizelo.prodGestor.modelo;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Setor {
@@ -17,6 +16,8 @@ public class Setor {
     
     private String nome;
     
+    @OneToMany(mappedBy = "setor")
+    private List<Usuario> usuarios;
 
     public Long getId() {
         return id;
--- prodGestor/src/main/java/com/frizelo/prodGestor/modelo/Usuario.java
+++ prodGestor/src/main/java/com/frizelo/prodGestor/modelo/Usuario.java
@@ -1,9 +1,15 @@
 package com.frizelo.prodGestor.modelo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Usuario {
@@ -11,6 +17,24 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotBlank(message = "O Nome do usuario deve ser informado")
+    @Size(min = 3, message = "O Nome deve ter no minimo 3 caracteres")
+    private String nome;
+    
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")    
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty(message = "A senha deve ser informada")
+	@Size(min = 3, message = "A senha deve ter no mínimo 3 caracteres")
+    private String password;
+
+    @ManyToOne
+    private Setor setor;
+
+    private boolean ativo;
 
     public Long getId() {
         return id;

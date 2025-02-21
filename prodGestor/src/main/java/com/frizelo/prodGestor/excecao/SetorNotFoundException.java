498eaaded4f8a984be584ebb988b7048de6f26a8
Pedro Pires <pirespedro020@gmail.com>
2025-02-21 08:42:39 -0400

    feat: create Setor exception

---
+++ prodGestor/src/main/java/com/frizelo/prodGestor/excecao/SetorNotFoundException.java
@@ -0,0 +1,8 @@
+package com.frizelo.prodGestor.excecao;
+
+public class SetorNotFoundException extends Exception {
+    
+    public SetorNotFoundException(String message) {
+        super(message);
+    }
+}

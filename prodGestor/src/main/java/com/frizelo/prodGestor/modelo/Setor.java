1c53a287ad87a75ba9968a4cf916b0f8141de8de
Pedro Pires <pirespedro020@gmail.com>
2025-02-21 08:40:13 -0400

    feat: crate Setor entity

---
+++ prodGestor/src/main/java/com/frizelo/prodGestor/modelo/Setor.java
@@ -0,0 +1,37 @@
+package com.frizelo.prodGestor.modelo;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Setor {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}

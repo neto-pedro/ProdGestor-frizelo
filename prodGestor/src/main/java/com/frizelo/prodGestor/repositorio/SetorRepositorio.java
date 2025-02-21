778c0a844fb6186dc33405cd815c9fd1f637df4a
Pedro Pires <pirespedro020@gmail.com>
2025-02-21 08:41:10 -0400

    feat: create Setor repositorio

---
+++ prodGestor/src/main/java/com/frizelo/prodGestor/repositorio/SetorRepositorio.java
@@ -0,0 +1,9 @@
+package com.frizelo.prodGestor.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.frizelo.prodGestor.modelo.Setor;
+
+public interface SetorRepositorio extends JpaRepository<Setor,Long>{
+
+}

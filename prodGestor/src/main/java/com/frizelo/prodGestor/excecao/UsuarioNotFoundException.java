84387c9cb2523760861a71d407261a092ce1bda3
neto-pedro <pirepedro020@gmail.com>
2025-02-25 12:32:39 -0400

    feat : criando Usuario exception

---
+++ prodGestor/src/main/java/com/frizelo/prodGestor/excecao/UsuarioNotFoundException.java
@@ -0,0 +1,8 @@
+package com.frizelo.prodGestor.excecao;
+
+public class UsuarioNotFoundException extends Exception {
+
+    public UsuarioNotFoundException(String message) {
+        super(message);
+    }
+}
